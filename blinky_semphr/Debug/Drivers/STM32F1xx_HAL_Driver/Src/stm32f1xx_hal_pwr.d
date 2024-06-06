c5ca7b34216796b98210253199ccb61b9a8c04c1
Marcos-Alcat <marcosalcat@gmail.com>
2024-06-05 21:04:30 -0300

    sensor code

---
+++ blinky_semphr/Debug/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.d
@@ -0,0 +1,74 @@
+Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.o: \
+ ../Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c \
+ C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ ../Core/Inc/stm32f1xx_hal_conf.h \
+ C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Inc/stm32f1xx.h \
+ C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Inc/stm32f103xb.h \
+ ../Drivers/CMSIS/Include/core_cm3.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Inc/system_stm32f1xx.h \
+ C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h \
+ C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
+ C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h
+
+C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+../Core/Inc/stm32f1xx_hal_conf.h:
+
+C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Inc/stm32f1xx.h:
+
+C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Inc/stm32f103xb.h:
+
+../Drivers/CMSIS/Include/core_cm3.h:
+
+../Drivers/CMSIS/Include/cmsis_version.h:
+
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Inc/system_stm32f1xx.h:
+
+C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h:
+
+C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
+
+C:/Users/Marcos/Documents/TD3/work/WS_STM32/freertos/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
